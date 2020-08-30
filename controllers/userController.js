d23622783b50eb4e8c2220b0e38670f15c9d4d1f
Mark_Zavatskyi <markzavatskyi@gmail.com>
2020-08-30 13:16:24 +0300

    add functionality to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,16 +2,32 @@ const { validationResult } = require('express-validator')
 const User = require('../models/User')
 
 module.exports.create = (req, res) => {
+    const errors = validationResult(req)
 
-    return res.status(201).json({ message: 'User successfully created!' })
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, name } = req.body
+
+    const user = new User(email, name)
+    user.save()
+
+    return res.status(201).json({ message: 'User successfully created!', user })
 }
 
 module.exports.getAll = (req, res) => {
-
-    return res.status(200).json({ message: 'Get All' })
+    const users = User.getAll()
+    return res.status(200).json(users)
 }
 
 module.exports.getOne = (req, res) => {
+    const { id } = req.params
+    const user = User.findById(id)
 
-    return res.status(200).json({ message: 'Get One' })
+    if(!user) {
+        return res.status(400).json({ message: `User with id ${id} not found!` })
+    }
+
+    return res.status(200).json(user)
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const userRouter = require('./routes/users')
 
 const app = express()
 const PORT = process.env.PORT || 8181
 
 app.use(express.json({ extended: true }))
+
+app.use('/api/users', userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server has been started on localhost:${PORT}...`)
