ba0ede9f3738cdc35333d205d0250ec7756df3d5
Mark_Zavatskyi <markzavatskyi@gmail.com>
2020-08-30 13:01:51 +0300

    project init

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8181
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on localhost:${PORT}...`)
+})
